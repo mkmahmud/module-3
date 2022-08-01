dc72429bcd000ae0267d4d5a0c266805828f74a8
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-07-31 21:45:42 -0700

    module1

---
+++ module1/pra.js
@@ -0,0 +1,40 @@
+// H.W 1
+
+var momGives = 1000;
+var apple = 400;
+var mango = 300;
+
+var totalCost = apple + mango;
+var remain = momGives - totalCost;
+
+// console.log(remain);
+
+// H.W 2
+
+var Bangla = 70;
+var English = 90;
+var Math = 60;
+var Islam = 93;
+var Ict = 84;
+
+var totalNum = Bangla+English+Math+Islam+Ict;
+var avr = totalNum/5;
+// console.log(avr);
+
+// H.W 3
+
+var teacher1 = "I am going to be a";
+var teacher2 = "an aewsome web developer";
+
+var mainSen = teacher1 +' '+teacher2;
+// console.log(mainSen);
+
+
+// H.W 4
+
+var saraMom = 119;
+var saraMomTo = 5;
+
+var modulas = saraMom%saraMomTo;
+
+console.log(modulas);
---
+++ module1/varyable.js
@@ -0,0 +1,25 @@
+// var mywallet = 300;
+
+// var myroll = 216;
+
+
+// myroll += 100;
+
+
+// myroll -= 50;
+// console.log(myroll);
+
+// myroll++;
+
+// var age = '20';
+// var you = 90;
+// console.log(parseInt(age)+you);
+
+var num1 = 0.1;
+var num2 = 0.2;
+
+var sum = num1+num2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+
+console.log(sum);
